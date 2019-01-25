7262f6848236cc592414750d918860afe8b2f9f6
Josh Halvorson <Jhalvorson5@mail.greenriver.edu>
2019-01-25 08:19:12 -0800

    Added test for getting display name. Added constants.

--- app/src/test/java/com/lambdaschool/congressdata/OfficialOverviewTest.java
+++ app/src/test/java/com/lambdaschool/congressdata/OfficialOverviewTest.java
@@ -1,22 +1,32 @@
 package com.lambdaschool.congressdata;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class OfficialOverviewTest {
 
+    public static final String FIRST_NAME = "firstName";
+    public static final String MIDDLE_NAME = "middleName";
+    public static final String LAST_NAME = "lastName";
+    public static final String PARTY = "party";
+    public static final String STATE = "state";
+    public static final String ID = "id";
     OfficialOverview officialOverview;
 
     @Before
     public void setUp() throws Exception {
-        officialOverview = new OfficialOverview(
-                        "firstName", "middleName",
-                "lastName", "party", "state", "id");
+        officialOverview = new OfficialOverview(FIRST_NAME, MIDDLE_NAME, LAST_NAME, PARTY, STATE, ID);
 
     }
 
-    
+    @Test
+    public void shouldGetDisplayName(){
+        String name = "firstName";
+
+        assertEquals(FIRST_NAME, name);
+    }
 
 
 }
