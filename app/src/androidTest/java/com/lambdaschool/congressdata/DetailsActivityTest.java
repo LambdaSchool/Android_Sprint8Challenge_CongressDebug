a1b37863c7cce16dedf025308e6c98a6a869f428
Josh Halvorson <Jhalvorson5@mail.greenriver.edu>
2019-01-25 08:47:49 -0800

    Added test for displaying correct congressperson name in details view.

--- app/src/androidTest/java/com/lambdaschool/congressdata/DetailsActivityTest.java
+++ app/src/androidTest/java/com/lambdaschool/congressdata/DetailsActivityTest.java
@@ -4,9 +4,14 @@ import android.content.Intent;
 
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 
 import androidx.test.rule.ActivityTestRule;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 public class DetailsActivityTest {
@@ -21,5 +26,10 @@ public class DetailsActivityTest {
         Intent intent = new Intent();
         intent.putExtra("id", "A000372");
         activityTestRule.launchActivity(intent);
+    }
+
+    @Test
+    public void shouldDisplayFullName(){
+        onView(withId(R.id.profile_name)).check(matches(withText("Rick Allen")));
     }
 }
