05d72eb088cee24df92c9ba1d2a67aff5a8629e3
Pablo Ogata <pabloogata@gmail.com>
2019-02-12 18:02:20 -0500

    unite Test

--- app/src/test/java/com/lambdaschool/congressdata/OfficialOverviewTest.java
+++ app/src/test/java/com/lambdaschool/congressdata/OfficialOverviewTest.java
@@ -1,5 +1,10 @@
 package com.lambdaschool.congressdata;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class OfficialOverviewTest {
 
 
@@ -12,4 +17,47 @@ public class OfficialOverviewTest {
     private static  final String STATE = "state";
     private static  final String DISPLAY_NAME = "displayName";
     private static  final String ID = "id";
+
+
+    @Before
+    public void setUp() {
+        officialOverview = new OfficialOverview("firstName", "middleName", "lastNamee", "party", "state","id");
+    }
+
+    @Test
+    public void getDisplayName() {
+
+        String name = "firstName";
+
+        assertEquals(name, officialOverview.getDisplayName());
+    }
+    @Test
+    public void buildDisplayName(){
+        String fullName = officialOverview.buildDisplayName();
+
+        assertEquals("firstName middleName lastName", fullName);
+    }
+    @Test
+    public void shouldGetId(){
+        String id = "id";
+
+        assertEquals(ID,  officialOverview id);
+    }
+
+    @Test
+    public void GetParty(){
+        String party = "party";
+
+        assertEquals(PARTY, party);
+    }
+
+    @Test
+    public void shouldGetState(){
+        String state = "state";
+
+        assertEquals(STATE, state);
+    }
+
+
+
 }
