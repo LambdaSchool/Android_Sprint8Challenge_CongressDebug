0d48222801f8e2cc3eacfd88cd5b2f58c01e7032
Pablo Ogata <pabloogata@gmail.com>
2019-02-12 17:29:10 -0500

    officialTestOverview

---
+++ app/src/test/java/com/lambdaschool/congressdata/OfficialOverviewTest.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.congressdata;
+
+public class OfficialOverviewTest {
+
+
+    private OfficialOverview officialOverview;
+
+    private static  final String FIRST_NAME = "firstName";
+    private static  final String MIDDLE_NAME = "middleName";
+    private static  final String LAST_NAME = "lastName";
+    private static  final String PARTY = "party";
+    private static  final String STATE = "state";
+    private static  final String DISPLAY_NAME = "displayName";
+    private static  final String ID = "id";
+}
