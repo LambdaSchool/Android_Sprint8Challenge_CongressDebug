96771fe2b83e42fbdd5e9509907d3bb87f514803
heiligbasil <basilief@yahoo.com>
2019-05-10 13:13:34 -0700

    Updated UI Tests to handle a CongresspersonProfile object

--- app/src/androidTest/java/com/lambdaschool/congressdata/DetailsActivityTest.java
+++ app/src/androidTest/java/com/lambdaschool/congressdata/DetailsActivityTest.java
@@ -1,33 +1,23 @@
 package com.lambdaschool.congressdata;
 
-import android.content.Context;
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import android.app.Activity;
 import android.content.Intent;
 
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.assertion.ViewAssertions;
-import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Before;
 import org.junit.Rule;
-import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import static org.junit.Assert.*;
-
 
 public class DetailsActivityTest {
 
-    private OfficialOverview officialOverview;
+    private CongresspersonProfile congresspersonProfile;
 
     @Rule
     public ActivityTestRule<DetailsActivity> detailsActivityTestRule = new ActivityTestRule<>(DetailsActivity.class, false, false);
@@ -35,31 +25,34 @@ public class DetailsActivityTest {
     @Before
     public void setUp() {
 
-        officialOverview = new OfficialOverview("Jaime", "Herrera", "Beutler", "Republican", "WA - District 3", "H001056");
+        congresspersonProfile = new CongresspersonProfile("Collin", "C.", "Peterson", "D",
+                "MN", "P000258", "CCP",
+                "1123456", "7", "202-225-3361",
+                "50", "654", "75");
 
         Intent intent = new Intent();
-        intent.putExtra("id", officialOverview.getId());
+        intent.putExtra("id", congresspersonProfile.getId());
 
         detailsActivityTestRule.launchActivity(intent);
     }
 
     @Test
     public void detailsActivity_DisplayName_ReturnsTrue() {
-        onView(withId(R.id.profile_name)).check(matches(withText(officialOverview.getDisplayName())));
+        onView(withId(R.id.profile_name)).check(matches(withText(congresspersonProfile.getDisplayName())));
     }
 
     @Test
     public void detailsActivity_DisplayParty_ReturnsTrue() {
-        onView(withId(R.id.profile_party)).check(matches(withText(officialOverview.getParty())));
+        onView(withId(R.id.profile_party)).check(matches(withText(congresspersonProfile.getParty())));
     }
 
     @Test
     public void detailsActivity_DisplayState_ReturnsTrue() {
-        onView(withId(R.id.profile_district)).check(matches(withText(officialOverview.getState())));
+        onView(withId(R.id.profile_district)).check(matches(withText(congresspersonProfile.getLocation())));
     }
 
     @Test
-    public void detailsActivity_DisplaPhone_ReturnsTrue() {
-        onView(withId(R.id.profile_phone)).check(matches(withText("202-225-3536")));
+    public void detailsActivity_DisplayPhone_ReturnsTrue() {
+        onView(withId(R.id.profile_phone)).check(matches(withText("202-225-2165")));
     }
 }
