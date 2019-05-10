14c8cc6c46dc2ff7b60e9924f75d74359ddf723c
Israel dela Cruz <igdcruz42@gmail.com>
2019-05-10 11:14:13 -0500

    added ui test for R.id.profile_name

---
+++ app/src/androidTest/java/com/lambdaschool/congressdata/DetailsActivityTest.java
@@ -0,0 +1,66 @@
+package com.lambdaschool.congressdata;
+
+import android.content.Intent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.assertion.ViewAssertions;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.rule.ActivityTestRule;
+
+
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+/**
+ * firstName: Ralph
+ * middleName: null
+ * lastName: Abraham
+ * district: 5
+ * facebookAccount: CongressmanRalphAbraham
+ * location: LA - District 5
+ * missedVotesPct: 36.73
+ * office: 417 Cannon House Office Building
+ * party: Republican
+ * phone: 202-225-8490
+ * state: LA
+ * twitterAccount: RepAbraham
+ * votesWithPartyPct: 97.56
+ *
+ * subcommittees: 8
+ * 	Conservation and Forestry
+ * 	General Farm Commodities and Risk Management
+ * 	Military Personnel
+ * 	Intelligence and Emerging Threats and Capabilities
+ * 	Seapower and Projection Forces
+ * 	Research and Technology
+ * 	Space and Aeronautics
+ * 	Investigations and Oversight
+ * */
+public class DetailsActivityTest {
+
+    @Rule
+    public ActivityTestRule<DetailsActivity> detailsActivityTestRule = new ActivityTestRule<DetailsActivity>(DetailsActivity.class) {
+        @Override
+        protected Intent getActivityIntent() {
+            Intent intent = new Intent();
+            intent.putExtra("id", "A000374");
+            return intent;
+        }
+    };
+
+    @Test
+    public void profileNameTextView_Success() {
+        onView(withId(R.id.profile_name)).check(matches(withText("Ralph Abraham")));
+
+    }
+
+
+}
