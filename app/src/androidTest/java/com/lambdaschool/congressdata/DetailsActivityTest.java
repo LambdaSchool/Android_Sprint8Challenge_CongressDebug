5c828c1a16f68a4073b821240a4a9d1afe232d49
Shoonsk8 <shoon@att.net>
2019-05-17 22:29:10 -0400

    test UI to facebook

--- app/src/androidTest/java/com/lambdaschool/congressdata/DetailsActivityTest.java
+++ app/src/androidTest/java/com/lambdaschool/congressdata/DetailsActivityTest.java
@@ -1,22 +1,42 @@
 package com.lambdaschool.congressdata;
 
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.rule.ActivityTestRule;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.AllOf.allOf;
 import static org.junit.Assert.*;
 
 public class DetailsActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> mainActivityTestRule = new ActivityTestRule<>(MainActivity.class, false, true);
 
+    @Before
+    public void setUp() throws Exception {
 
-    @Test
-    public void onCreate() {
+        Intents.init();
+        onView( allOf( withId( R.id.text_list_name ), withText( "Alma Adams" ) ) ).perform( click() );
     }
 
     @Test
+    public void testFacebook() {
+        onView(withId( R.id.profile_facebook )).perform( click() );
+
+    }
+
+    @After
     public void onStart() {
+
+        Intents.release();
     }
 }
